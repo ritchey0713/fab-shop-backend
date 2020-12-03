bb01bc1a0f918516bb20d8f15eecd4691157d30d
Dj <ritchey0713@gmail.com>
2020-12-03 11:45:39 -0500

    create new client strategy

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
+passport.use(new GoogleStrategy());
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
