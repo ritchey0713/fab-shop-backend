13d52cff46bed6fee472d7477da7f41b373a6ae4
Dj <ritchey0713@gmail.com>
2020-12-09 12:29:26 -0500

    add connection to mongoose

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+// fire off google strategy
 require("./services/passport");
-
+const keys = require("./config/keys");
 const app = express();
 
 require("./routes/authRoutes")(app);
-
 const PORT = process.env.PORT || 5000;
 
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(keys.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+  } catch (err) {
+    console.log("connection failed", err.message);
+    process.exit(1);
+  }
+};
+
+dbConnection();
 app.listen(PORT);
