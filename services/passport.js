f3243ba3326af517bf66e1899ab1c2dc74ee78f4
ritchey0713 <ritchey0713@gmail.com>
2020-12-17 16:07:51 -0500

    clean up passport.js file

--- services/passport.js
+++ services/passport.js
@@ -26,11 +26,10 @@ passport.use(
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleId: profile.id });
       if (existingUser) {
-        done(null, existingUser);
-      } else {
-        const newUser = await new User({ googleId: profile.id }).save();
-        done(null, newUser);
+        return done(null, existingUser);
       }
+      const newUser = await new User({ googleId: profile.id }).save();
+      done(null, newUser);
     }
   )
 );
