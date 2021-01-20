bff5b8db9f12655134ba56a97de119f44bb259a6
ritchey0713 <ritchey0713@gmail.com>
2021-01-20 11:45:18 -0500

    add schemas

--- routes/fabRoutes.js
+++ routes/fabRoutes.js
@@ -5,7 +5,7 @@ const Fab = mongoose.model("fabRequests");
 module.exports = (app) => {
   app.post("/api/requests", requireLogin, async (req, res) => {
     try {
-      console.log(req.body);
+      console.log(" in post ");
       const newFabRequest = await new Fab(req.body).save();
       res.status(200).send(newFabRequest);
     } catch (err) {
