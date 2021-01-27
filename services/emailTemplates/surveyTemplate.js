0271ec8753a1a1b06787bc343111d5af9021beb5
ritchey0713 <ritchey0713@gmail.com>
2021-01-27 16:06:53 -0500

    build survey template

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,19 @@
 module.exports = (survey) => {
-  return `<div>${survey.body}</div>`;
+  return `
+    <html>
+      <body>
+        <div style="text-align: center">
+          <h3>
+            We'd love your feedback!
+          </h3>
+          <p>
+            Please answer the following question:
+          </p>
+          <p>
+            ${survey}
+          </p>
+        </div>
+      </body>
+    </html>
+    `;
 };
