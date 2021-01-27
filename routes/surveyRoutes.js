ec923d05345a8e707015cc604bce372f7608498a
ritchey0713 <ritchey0713@gmail.com>
2021-01-27 10:34:18 -0500

    add new sendgrid lib

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const requireCredits = require("../middlewares/requireCredits");
 const requireLogin = require("../middlewares/requireLogin");
 const Survey = mongoose.model("surveys");
+const sendMail = require("../services/Mailer");
+const template = require("../services/emailTemplates/surveyTemplate");
 
 module.exports = (app) => {
   app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
@@ -15,5 +17,8 @@ module.exports = (app) => {
       _user: req.user.id,
       dateSent: Date.now(),
     });
+    // send emails
+    // const mailer = new Mailer(survey, template(survey))
+    // sendMail(template(survey), subjectt, recipients)
   });
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -1,2 +1,26 @@
-const sendgrid = require("sendgrid");
-const helper = sendgrid;
+// const sendgrid = require("sendgrid");
+// const helper = sendgrid;
+
+// class Mailer extends helper.Mail {}
+
+// module.exports = Mailer;
+
+const sgMail = require("@sendgrid/mail");
+const keys = require("../config/keys");
+sgMail.setApiKey(keys.sendGridKey);
+
+module.exports = async (body, subject, recipients) => {
+  const msg = {
+    from: keys.mailFrom,
+    subject,
+    text: body,
+    personalizations: recipients.map((recipient) => ({ to: [recipient] })),
+  };
+
+  try {
+    const result = await sgMail.send(msg);
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
